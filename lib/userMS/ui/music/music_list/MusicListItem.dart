4136d0997238674f31ec8645ad553cfcf9bc64d5
vietahh <vietahh02@gmail.com>
2026-01-21 17:52:55 +0700

    a

--- lib/userMS/ui/music/music_list/MusicListItem.dart
+++ lib/userMS/ui/music/music_list/MusicListItem.dart
@@ -9,18 +9,70 @@ class MusicListItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-      padding: EdgeInsets.all(16),
+      // padding: EdgeInsets.all(16),
       decoration: BoxDecoration(
         color: Colors.white,
         borderRadius: BorderRadius.circular(12),
       ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(music.title),
-          SizedBox(width: 16),
-          Text(music.artist),
-        ],
+      child: ListTile(
+        contentPadding: EdgeInsets.only(left: 16, right: 0),
+        trailing: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            IconButton(
+              icon: Icon(Icons.play_arrow),
+              onPressed: () {
+                print('Play pressed');
+              },
+            ),
+            IconButton(
+              icon: Icon(Icons.more_vert),
+              onPressed: () {
+                print('More pressed');
+                showDialog(context: context, builder: (context) => AlertDialog(
+                  title: Text('More'),
+                  content: Text('Are you sure you want to more this music?'),
+                  actions: [
+                    TextButton(onPressed: () => Navigator.pop(context), child: Text('Cancel')),
+                    TextButton(onPressed: () => Navigator.pop(context), child: Text('More')),
+                  ],
+                ));
+              },
+            ),
+          ],
+        ),
+        leading: ClipRRect(
+          borderRadius: BorderRadius.circular(12),
+          child: FadeInImage(
+            placeholder: AssetImage("assets/placeholder.jpg"),
+            image: NetworkImage(music.image),
+            width: 50,
+            height: 50,
+            fit: BoxFit.cover,
+            imageErrorBuilder: (context, error, stackTrace) {
+              return Image.asset('assets/placeholder.jpg', width: 50, height: 50, fit: BoxFit.cover,);
+            }
+          ),
+        ),
+        title: Text(music.title,
+          style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600, color: Colors.grey.shade800),
+          overflow: TextOverflow.ellipsis,
+          maxLines: 1,
+        ),
+        subtitle: Row(
+          children: [
+            Icon(Icons.music_video),
+            SizedBox(width: 8),
+            Expanded(
+              child: Text(
+                music.artist,
+                style: TextStyle(fontSize: 14, color: Colors.grey.shade600),
+                overflow: TextOverflow.ellipsis,
+                maxLines: 1,
+              )
+            )
+          ],
+        ),
       ),
     );
   }
